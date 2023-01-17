d7ebe688b9893adc86e277b5340b805a8f8d8781
Hassan Ameer <118745053+HassanAmeer@users.noreply.github.com>
2023-01-17 20:13:25 +0500

    assets

---
+++ assets/js/sec.js
@@ -0,0 +1,50 @@
+const linkItems = document.querySelectorAll(".link-item");
+
+linkItems.forEach((linkItem, index) => {
+    linkItem.addEventListener("click", () => {
+        document.querySelector(".active").classList.remove("active");
+        linkItem.classList.add("active");
+
+        const indicator = document.querySelector(".indicator");
+
+        indicator.style.left = `${index * 95 + 48}px`;
+    })
+})
+
+function bnavbtnhom(){
+    setTimeout (function(){
+        window.location.href = 'home.php';
+    }, 1000);
+}
+function bnavabtnbuy(){
+    setTimeout (function(){
+        window.location.href = 'buy.php';
+    }, 1000);
+}
+function bnavbtnhstry(){
+    setTimeout (function(){
+        window.location.href = 'history.php';
+    }, 1000);
+}
+function bnavabtnprof(){
+    setTimeout (function(){
+        window.location.href = 'profile.php';
+    }, 1000);
+}
+function bnavabtnexit(){
+    setTimeout (function(){
+        window.location.href = 'sumout.php';
+    }, 1000);
+}
+
+
+///////
+
+
+
+
+
+
+
+
+
